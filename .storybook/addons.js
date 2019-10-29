30fafe48cdcc8f7bf4cc3f9c003ea2b93528e3e2
g-alcorn <griffin.alcorn@gmail.com>
2019-10-29 12:05:01 +0000

    button counter enabled

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,11 @@
 import "@storybook/addon-actions/register";
 import "@storybook/addon-backgrounds/register";
+
+// import Button from '../src/components/Button.js';
+
+// const eventFromObject = actions({ onClick: 'clicked', onMouseOver: 'hovered '});
+
+// storiesOf('Button', module)
+//   .add('clicked', () => (
+//     <Button {...eventFromObject}>Hello World!</Button>
+//   ));
