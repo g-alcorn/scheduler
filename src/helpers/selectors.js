3f0d6eb1291bf2783f3a6004b8d75c214302722e
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-03 17:19:05 -0500

    helper function implemented to get appointments for given day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,35 @@
+export function getAppointmentsForDay(state, day) {
+  let dayIndex = null;
+  let response = [];
+  switch (day) {
+    case "Monday":
+      dayIndex = 0;
+      break;
+    case "Tuesday":
+      dayIndex = 1;
+      break;
+    case "Wednesday":
+      dayIndex = 2;
+      break;
+    case "Thursday":
+      dayIndex = 3;
+      break;
+    case "Friday":
+      dayIndex = 4;
+      break;
+    default:
+      return [];
+  }
+
+  if (state.days[dayIndex]) {
+    for (let i = 0; i < state.days[dayIndex].appointments.length; i++) {
+      for (let appt in state.appointments) {
+        if (state.appointments[appt].id === state.days[dayIndex].appointments[i]) {
+          response.push(state.appointments[appt]);
+        }
+      }
+    }    
+  }
+
+  return response;
+};
