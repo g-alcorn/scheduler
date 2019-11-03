54ad668e3d1d6a2ccdf1faaba5e6d5a75082dbb1
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-03 17:20:07 -0500

    makes axios calls and singular state object containing multiple data types

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,76 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
-import Appointment from "./Appointment/index"
+
+import { getAppointmentsForDay } from "../helpers/selectors";
+import Appointment from "./Appointment/index";
 import DayList from "components/DayList";
 
-//DUMMY DATA TO FILL DAY LIST
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-//DUMMY DATA FOR APPOINTMENTS
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Griffin Alcorn",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png"
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Alcorn Griffin",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png"
-      }
-    }
-  }
-];
-
-
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const response = appointments.map((appointment) => 
+  //INITIALIZE EMPTY DAY LIST IN PROGRAM STATE
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  //const setDays = days => setState(prev => ({ ...prev, days }))
+
+  //GETS DAYS ONCE UPON PAGE LOAD
+  useEffect(() => {
+    Promise.all([
+      axios.get(`http://localhost:8001/api/days`),
+      axios.get(`http://localhost:8001/api/appointments`)
+    ]).then((all) => {
+      console.log(all[0]); // first
+      console.log(all[1]); // second
+    
+      const [days, appointments] = all;
+      setState(prev => ({ days: days.data, appointments: appointments.data}));
+    });
+  }, []);
+
+  const response = getAppointmentsForDay(state, state.day).map((appointment) => 
     <Appointment 
       key={appointment.id}
       {...appointment}
@@ -89,8 +52,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
             <DayList
-              days={days}
-              day={day}
+              days={state.days}
+              day={state.day}
               setDay={setDay}
             />
 
