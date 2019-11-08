76b40a9dc59ea5ffbbc9b5f752bf1fcbd76c9051
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-08 11:50:04 -0500

    implementing useApplicationData hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,7 +20,6 @@ export default function Application(props) {
   
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-    
     return (
     <Appointment 
       key={appointment.id}
